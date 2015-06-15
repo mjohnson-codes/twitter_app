8354ef23f7ae5f529975ce23d047e34c50a034fc
mjohnson-codes <mjohnson@trialnetwork.com>
2015-06-15 18:45:10 -0400

    fixed user.rb issue line 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :tweets
-	validates :username, presence: true, uniqueness: true, { maximum: 20}
+	validates :username, presence: true, uniqueness: true, length: {maximum: 20}
 	validates :password, length: { in: 6..12 }, presence: true
 end
